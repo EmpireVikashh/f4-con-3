e49ee4b32c44b4ed8623a252bd4fcba82af9a6db
Vikash sharma <116377338+empirevikashh@users.noreply.github.com>
2024-01-29 07:11:48 +0000

    Just started

--- src/App.js
+++ src/App.js
@@ -1,40 +1,75 @@
-// App.js
-import React, { useState } from "react";
-import Login from "./Components/Login";
-import Profile from "./Components/Profile";
+import React from 'react'
 
 const App = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [userId, setUserId] = useState(null);
-
-  const handleLogin = () => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (user) {
-      setLoggedIn(true);
-      setUserId(user.id);
-    }
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem("user");
-    setLoggedIn(false);
-    setUserId(null);
-  };
-
   return (
-    <div className="container">
-      {loggedIn ? (
-        <>
-          <Profile userId={userId} />
-          <button className="logout-btn" onClick={handleLogout}>
-            Logout
-          </button>
-        </>
-      ) : (
-        <Login onLogin={handleLogin} />
-      )}
+    <div className=' bg'>
+      App
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// App.js
+// import React, { useState } from "react";
+// import Login from "./Components/Login";
+// import Profile from "./Components/Profile";
+
+// const App = () => {
+//   const [loggedIn, setLoggedIn] = useState(false);
+//   const [userId, setUserId] = useState(null);
+
+//   const handleLogin = () => {
+//     const user = JSON.parse(localStorage.getItem("user"));
+//     if (user) {
+//       setLoggedIn(true);
+//       setUserId(user.id);
+//     }
+//   };
+
+//   const handleLogout = () => {
+//     localStorage.removeItem("user");
+//     setLoggedIn(false);
+//     setUserId(null);
+//   };
+
+//   return (
+//     <div className="container">
+//       {loggedIn ? (
+//         <>
+//           <Profile userId={userId} />
+//           <button className="logout-btn" onClick={handleLogout}>
+//             Logout
+//           </button>
+//         </>
+//       ) : (
+//         <Login onLogin={handleLogin} />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default App;
