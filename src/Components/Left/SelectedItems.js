7926e28aca95cd34299cae00307e2728de21ce7a
Vikash <empirevikash@gmail.com>
2024-01-30 22:21:26 +0530

    haveToAddItem

--- src/Components/Left/SelectedItems.js
+++ src/Components/Left/SelectedItems.js
@@ -3,11 +3,16 @@ import React from "react";
 const SelectedItems = () => {
   return (
     <div className="items flex flex-wrap justify-center">
-      Click to Select
-      {Array(0)
+      <p>Click to Select</p>
+      {Array(1)
         .fill(null)
         .map((_, index) => (
-          <div key={index} className="bg-lime-600 p-4 m-1 text-center hover:bg-red-500"></div>
+          <div
+            key={index}
+            className="bg-lime-600 p-4 m-1 text-center hover:bg-red-500"
+          >
+            {index}
+          </div>
         ))}
     </div>
   );
