0957fdb923185fa69c0c6da05cc31a89be8f72b5
Vikash <empirevikash@gmail.com>
2024-01-30 17:53:24 +0530

    sizeSet

--- src/Components/Left/LeftSection.js
+++ src/Components/Left/LeftSection.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import SelectedItems from "./SelectedItems";
 
 const LeftSection = () => {
-  const sizes = ["10x10", "20x20", "30x30"];
+  const sizes = ["10x10", "20x20", "30x30", "40x40"];
+  const arrSize = [10,20,30,40];
+  const [selectedContainer, setSelectedContainer] = useState(sizes);
+  const [containerSize, setSContainerSize] = useState(10);
+
+  const handleSelectedContainer = (size,arraySize) => {
+    setSelectedContainer(size ? [size] : sizes);
+    setSContainerSize(arraySize)
+    // console.log(size ? size + " True" : size + " false");
+  };
+  console.log(containerSize);
   return (
-    <section className="leftSection border-2 border-red-200 rounded-md w-64 p-1">
-      <h1 className=" bg-slate-400 p-2 text-center">Available Container</h1>
+    <section className="leftSection border-2 border-red-200 rounded-md w-64 p-1 h-[96vh] overflow-hidden hover:overflow-auto">
+      <h1 className=" bg-slate-400 p-2 text-center rounded-md">
+        Available Container
+      </h1>
+      <h2
+        className=" bg-green-700 p-2 text-center rounded-full mt-2 hover:bg-red-600 cursor-pointer"
+        onClick={()=>handleSelectedContainer(false)}
+      >
+        Show All Containers
+      </h2>
       <div className="availableContainers px-1">
-        {sizes.map((size) => (
-          <div key={size} className="containers bg-yellow-600 mt-2 rounded-md cursor-pointer hover:brightness-90">
-           <h1 className="font-semibold bg-red-300 border border-blue-500 text-center">{size}</h1>
-           <div className="items">
-            <SelectedItems/>
-           </div>
+        {selectedContainer.map((size, idx) => (
+          <div
+            key={size}
+            className="containers bg-yellow-600 mt-2 rounded-md cursor-pointer hover:brightness-90 hover:bg-green-500"
+            onClick={() => handleSelectedContainer(size,arrSize[idx])}
+          >
+            <h1 className="font-semibold bg-red-300 border border-blue-500 text-center">
+              {size}
+            </h1>
+            <div className="items p-6">
+              <SelectedItems />
+            </div>
           </div>
         ))}
       </div>
--- src/Components/Left/SelectedItems.js
+++ src/Components/Left/SelectedItems.js
@@ -3,10 +3,11 @@ import React from "react";
 const SelectedItems = () => {
   return (
     <div className="items flex flex-wrap justify-center">
-      {Array(8)
+      Click to Select
+      {Array(0)
         .fill(null)
         .map((_, index) => (
-          <div key={index} className="bg-lime-600 p-4 m-1 text-center hover:bg-red-500">{index}</div>
+          <div key={index} className="bg-lime-600 p-4 m-1 text-center hover:bg-red-500"></div>
         ))}
     </div>
   );
